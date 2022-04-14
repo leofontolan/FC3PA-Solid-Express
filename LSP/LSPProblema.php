91e96695f8cd2fa43cd0569dc26e0b5ff91e34a8
leofontolan <leofontolan@gmail.com>
2022-04-13 21:11:32 -0300

    Liskov Substitution

---
+++ LSP/LSPProblema.php
@@ -0,0 +1,34 @@
+<?php
+
+class Movie{
+
+
+    public function play(){
+        // play video
+    }
+
+    public function incrementVolume(){
+        // increment volume
+    }
+
+}
+
+class TheLionKing extends Movie{
+
+    public function play(){
+        // play video
+    }
+
+    public function incrementVolume(){
+        // increment volume
+    }
+
+}
+
+class ModernTimes extends Movie{
+
+    public function play(){
+        // play video
+    }
+
+}
